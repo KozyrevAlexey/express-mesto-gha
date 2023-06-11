ddfd8f5b4545e422db20071c339dafad58d49f04
Alexey Kozyrev <guper095@gmail.com>
2023-06-11 17:33:23 +0300

    feat: Собрать проект

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const router = require('./routes');
 
 const { PORT = 3000 } = process.env
 
 const app = express();
-
 app.use(express.json());
+
 
 /**временное решение авторизации */
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '6485b2d0bb42dd4438752f92' // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
 
   next();
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
-
-
-
-const users =[];
-let id = 0;
-
-app.get('/users', (reg, res) => {
-  console.log("Запрос на /users");
-
-  res.status(200).send(users);
-});
-
-app.post('/users', (reg, res) => {
-  id += 1;
-  const newUser = {
-    id,
-    ...reg.body,
-  };
-
-  users.push(newUser);
-
-  res.status(201).send(newUser);
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+app.use(router);
+app.use('/', (reg, res) => {
+  res.status(404).send({ message: 'Что-то пошло не так...'});
 });
 
 app.listen(PORT, () => {
-  console.log(`Слушаю порт ${PORT}` )
+  console.log(`Слушаю порт ${PORT}`)
 });
---
+++ controllers/users.js
@@ -0,0 +1,93 @@
+
+const User = require('../models/user')
+
+const { ERROR_VALIDATION, ERROR_NOT_FOUND, ERROR_DEFAULT } = require('../errors/errors');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .populate(['name', 'about', 'avatar'])
+    .then(users => res.send(users))
+    .catch(err => {
+      return res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then(user => {
+      res.send(user)
+    })
+    .catch(err => {
+      if (!name || !avatar || err.message) {
+        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
+        return;
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message })
+      }
+    });
+};
+
+const getUserBuId = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .then(user => {
+      if (!user) {
+        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь не найден` });
+        return;
+      }
+      res.send(user);
+    })
+    .catch(err => {
+      if (!User[userId]) {
+        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
+        return;
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message });
+      }
+    });
+};
+
+const updateProfileUser = (req, res) => {
+  const { name, about } = req.body;
+  const { _id } = req.user;
+  User.findByIdAndUpdate(_id, { name, about }, { new: true, runValidators: true })
+    .then(user => res.send(user))
+    .catch(err => {
+      if (!name || !about || err.message) {
+        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
+        return;
+      } else if (!User[_id]) {
+        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь не найден` });
+        return;
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message })
+      }
+    })
+};
+
+const updateAvatarUser = (req, res) => {
+  const { avatar } = req.body;
+  const { _id } = req.user;
+  User.findByIdAndUpdate(_id, { avatar }, { new: true, runValidators: true })
+    .then(user => res.send(user))
+    .catch(err => {
+      if (!User[_id]) {
+        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь не найден` });
+        return;
+      } else if (!avatar) {
+        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
+        return;
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message })
+      }
+    })
+}
+
+module.exports = {
+  getUsers,
+  createUser,
+  getUserBuId,
+  updateProfileUser,
+  updateAvatarUser,
+};
